134033a880e78ca2162db26cc4a1884924b5942c
Ruy Diaz <diazruy@gmail.com>
2016-03-31 08:59:01 -0700

    Provide own version of `Hash#deep_transform_key` instead of depending on RoR4
    WIP
    
    Need to find a better place for this code and perhaps even make it a non-monkey
    patch solution

--- lib/insightly2.rb
+++ lib/insightly2.rb
@@ -1,5 +1,15 @@
 require 'insightly2/version'
 
+class Hash
+  def deep_transform_keys(&block)
+    result = {}
+    each do |key, value|
+      result[yield(key)] = value.is_a?(Hash) ? value.deep_transform_keys(&block) : value
+    end
+    result
+  end
+end
+
 module Insightly2
   autoload :Client, 'insightly2/client'
   autoload :DSL, 'insightly2/dsl'
